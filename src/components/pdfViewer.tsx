29ad1c58d9c6d79103bc4fc1e36f9af8616bf2db
VietTriE <mkt.viettri@gmail.com>
2024-11-29 09:29:27 +0700

    pdf responsive

--- src/components/pdfViewer.tsx
+++ src/components/pdfViewer.tsx
@@ -1,15 +1,21 @@
    // src/components/PdfViewer.js
    import React from 'react';
    import { Worker, Viewer } from '@react-pdf-viewer/core';
+   import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
    import '@react-pdf-viewer/core/lib/styles/index.css';
    import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
    const PdfViewer = ({pdfUrl}: {pdfUrl: string}) => {
+       // Initialize the default layout plugin
+       const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
        return (
            <div className=" w-full h-full" style={{width: '100%', height: '100%'}} >
                <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-                   <Viewer fileUrl={pdfUrl} />
+                   <Viewer
+                       fileUrl={pdfUrl}
+                       plugins={[defaultLayoutPluginInstance]} // Use the default layout plugin
+                   />
                </Worker>
            </div>
        );
